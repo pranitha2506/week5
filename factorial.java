cf8a114b63e802ad77b2597b39081c72bf505f6a
pranitha2506 <padthampranitha2004@gmail.com>
2026-08-13 15:05:11 +0530

    Uncomment main method in factorial.java

--- factorial.java
+++ factorial.java
@@ -7,9 +7,9 @@ public class factorial{
             }
             return result;
         }
-        /*public static void main(String[] args){
+        public static void main(String[] args){
             int number=5;
             long factorial=compute(number);
             System.out.println("Factorial of "+number+ "is:"+factorial)
-        }*/
+        }
     }
