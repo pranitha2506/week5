ba7f25b602440cae1be36f958fb4bf0307866d22
pranitha2506 <padthampranitha2004@gmail.com>
2026-08-13 15:06:24 +0530

    Fix syntax error in print statement

--- factorial.java
+++ factorial.java
@@ -10,6 +10,6 @@ public class factorial{
         public static void main(String[] args){
             int number=5;
             long factorial=compute(number);
-            System.out.println("Factorial of "+number+ "is:"+factorial)
+            System.out.println("Factorial of "+number+ "is:"+factorial);
         }
     }
